e22912aee213c4df040cef05893e3978faa50564
jorge_antonio <jorge@metastage.app>
2022-01-19 23:17:06 +0000

    refactor code

--- meta/contracts/CrowdFundContract.sol
+++ meta/contracts/CrowdFundContract.sol
@@ -6,7 +6,7 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 
-contract  is Ownable {
+contract CrowdFundContract is Ownable {
     
     bool public startVotation;
     bool public endVotation;
