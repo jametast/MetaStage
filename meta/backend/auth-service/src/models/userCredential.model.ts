07b7715056c1a6c1c8e1fb780af78446df529fe4
Ubuntu <ubuntu@ip-172-31-22-176.us-east-2.compute.internal>
2022-02-26 20:59:21 +0000

    add user-credentials table

---
+++ meta/backend/auth-service/src/models/userCredential.model.ts
@@ -0,0 +1,30 @@
+import AWS from "aws-sdk";
+import { client, docClient } from "../db/dbConfig";
+
+
+let tableName = "user_credentials";
+
+let params = {
+    TableName: tableName,
+    KeySchema: [
+        { AttributeName: "publicKey", KeyType: "S"}  //Partition key; TODO - check type of partition key
+    ],
+    AttributeDefinitions: [
+        { AttributeName: "publicKey", AttributeType: "S" },
+        { AttributeName: "nonce", AttributeType: "N" },
+        { AttributeName: "lastLogIn", AttributeType: "S" },
+    ],
+    ProvisionedThroughput: {
+        ReadCapacityUnits: 10,
+        WriteCapacityUnits: 10
+    }
+};
+
+
+client.createTable(params, function(tableErr: AWS.AWSError) {
+    if (tableErr) {
+        console.error("Error JSON:", JSON.stringify(tableErr, null, 2));
+    } else {
+        console.log("Created table successfully!");
+    }
+});
