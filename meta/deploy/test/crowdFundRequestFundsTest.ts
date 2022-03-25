db3b6bba65cf7a976a15f78c28893c057fd2c307
Ubuntu <ubuntu@ip-172-31-80-213.ec2.internal>
2022-03-25 15:15:26 +0000

    add crowd fund request funds tests in ts

--- meta/deploy/test/crowdFundRequestFundsTest.ts
+++ meta/deploy/test/crowdFundRequestFundsTest.ts
@@ -1,49 +1,50 @@
-const { expect, assert } = require("chai");
-const { ethers } = require("hardhat");
-const { getCrowdFundContract, getCurrentBlockTimestamp } = require("./deployTest");
+import { expect, assert } from "chai";
+import { ethers } from "hardhat";
+import { BigNumber, Contract, providers, Signer } from "ethers";
+import { getCrowdFundContract, getCurrentBlockTimestamp } from "./deployTest";
 
 
 describe("CrowdFundRequestFunds", function () {
     it("Request funds", async function () {
 
       // get deployed crowd fund contract
-      const crowdFundContract = await getCrowdFundContract();
+      const crowdFundContract: Contract = await getCrowdFundContract();
       // get current block timestamp
-      let currentBlockTimestamp = await getCurrentBlockTimestamp();
+      let currentBlockTimestamp: number = await getCurrentBlockTimestamp();
 
       // get start time of request funds period
-      const startTimeRequestFunds = await crowdFundContract.startTimeRequestFunds();
+      const startTimeRequestFunds: number = await crowdFundContract.startTimeRequestFunds();
       // get end time of request funds period
-      const endTimeRequestFunds = await crowdFundContract.endTimeRequestFunds();
+      const endTimeRequestFunds: number = await crowdFundContract.endTimeRequestFunds();
 
       // how many seconds to start request funds period
-      let increaseTimeInSeconds = startTimeRequestFunds - currentBlockTimestamp + 1;
+      let increaseTimeInSeconds: number = startTimeRequestFunds - currentBlockTimestamp + 1;
       
       // increase timestamp in block by increaseTimeInSeconds
       await ethers.provider.send("evm_increaseTime", [increaseTimeInSeconds]);
       // mine new block
-      await ethers.provider.send("evm_mine");
+      await ethers.provider.send("evm_mine", []);
 
       // get bool value determining if we are in request funds period
-      const requestFundsPeriodStarted = await crowdFundContract.requestFundsStarted();
-      const requestFundsPeriodEnded = await crowdFundContract.requestFundsEnded();
-      const inRequestFundsPeriod = requestFundsPeriodStarted && !requestFundsPeriodEnded;
+      const requestFundsPeriodStarted:boolean = await crowdFundContract.requestFundsStarted();
+      const requestFundsPeriodEnded: boolean = await crowdFundContract.requestFundsEnded();
+      const inRequestFundsPeriod: boolean = requestFundsPeriodStarted && !requestFundsPeriodEnded;
       // guarantee we are in request funds period
       assert(inRequestFundsPeriod);
 
       // get owner and three other signers (ethers account abstraction)
-      const [owner, addr1, addr2, addr3] = await ethers.getSigners();
+      const [owner, addr1, addr2, addr3]: Signer[] = await ethers.getSigners();
 
       // get addr1 public key
-      const account1 = await addr1.getAddress();
+      const account1: string = await addr1.getAddress();
      
       // define funds to be requested
-      let fundsRequested = ethers.utils.parseEther('1.0');
+      let fundsRequested: BigNumber = ethers.utils.parseEther('1.0');
 
       // owner makes addr1 elligible to request funds
       await crowdFundContract.connect(owner).makeCreatorElligible(account1);
       // is account1 elligible to request funds to our contract
-      let accountIsElligibleBool = await crowdFundContract.isCreatorElligible(account1);
+      let accountIsElligibleBool: boolean = await crowdFundContract.isCreatorElligible(account1);
       // assert that addr1 public key belongs to contract elligible creators object
       assert(accountIsElligibleBool);
 
@@ -52,14 +53,14 @@ describe("CrowdFundRequestFunds", function () {
       // assert that addresToCreatorMapping mapping of our contract has been updated with addr1 public key
     
       // extract creator structure from contract
-      let creator = await crowdFundContract.addressToCreatorMapping(account1);
+      let creator: any = await crowdFundContract.addressToCreatorMapping(account1);
       // assert that creator's requestFunds BigNumber value equals fundsRequested 
       assert(creator.requestedFunds.eq(fundsRequested));
       // assert that creator's totalFunds BigNumber equals 0 for the time being
       assert(creator.totalFunds.eq(ethers.utils.parseEther('0.0')));
 
       // we now try to request funds again using account1 to our contract, it should exit with error
-      const requestAgainFunds = async () => {
+      const requestAgainFunds = async (): Promise<void> => {
         try {
           await crowdFundContract.connect(addr1).requestFunds(fundsRequested);
         } catch(err) {
@@ -70,12 +71,12 @@ describe("CrowdFundRequestFunds", function () {
       }
 
       // expect undefined value from calling requestAgainFunds
-      let expectUndefined = await requestAgainFunds();
+      let expectUndefined: void = await requestAgainFunds();
       expect(expectUndefined).to.be.a("undefined");
 
       // we now allow addr2 to request funds as well
       // get public key from addr2
-      const account2 = await addr2.getAddress();
+      const account2: string = await addr2.getAddress();
       // owner allows addr2 to request funds
       await crowdFundContract.connect(owner).makeCreatorElligible(account2);
 
@@ -99,8 +100,8 @@ describe("CrowdFundRequestFunds", function () {
       // we now test that addr3 user cannot request funds, given that it is not authorized by owner of smart contract
       
       // get addr3 public key
-      const account3 = await addr3.getAddress();
-      const notElligibleRequestFunds = async () => {
+      const account3: string = await addr3.getAddress();
+      const notElligibleRequestFunds = async (): Promise<void> => {
         try {
           await crowdFundContract.connect(addr3).requestFunds(fundsRequested);
         } catch(err) {
@@ -125,13 +126,13 @@ describe("CrowdFundRequestFunds", function () {
       // increase timestamp in block by increaseTimeInSeconds
       await ethers.provider.send("evm_increaseTime", [increaseTimeInSeconds]);
       // mine new block
-      await ethers.provider.send("evm_mine");
+      await ethers.provider.send("evm_mine", []);
 
       // assert that request fund period has already finished
-      const requestFundsPeriodEndedBool = await crowdFundContract.requestFundsEnded();
+      const requestFundsPeriodEndedBool: boolean = await crowdFundContract.requestFundsEnded();
       assert(requestFundsPeriodEndedBool);
       
-      const requireFundsOutPeriod = async () => {
+      const requireFundsOutPeriod = async (): Promise<void> => {
         try {
           await crowdFundContract.connect(addr3).requestFunds(fundsRequested);
         } catch(err) {
---
+++ meta/deploy/test/crowdFundTimelineTest.ts
@@ -0,0 +1,255 @@
+import { expect, assert } from "chai";
+import { ethers } from "hardhat";
+import { BigNumber, ContractFactory, Contract, providers } from "ethers";
+
+
+describe("CrowdFundContractTimeLine", function () {
+    it("Timeline testing of crowd funding contract", async function () {
+        // get contract factory
+        const CrowdFundContract: ContractFactory = await ethers.getContractFactory("CrowdFundContract");
+        
+        // get latest block number
+        let blockNumber: number = ethers.provider.blockNumber;
+        // get current block data
+        let currentBlockData: providers.Block = await ethers.provider.getBlock(blockNumber);
+        // get current block timestamp
+        let currentBlockTimestamp: number = currentBlockData["timestamp"];
+        
+        // get current timestamp in seconds 
+        const nowInSeconds: number = currentBlockTimestamp + 1 // prior, we were testing it with date now method:  Math.floor(Date.now() / 1000);
+        // starts in 2 minutes
+        const startRequestFunds: number = nowInSeconds + 60 * 2 
+        // requesting funds will have a duration of 5 minutes
+        const endRequestFunds: number = nowInSeconds + 60 * (2 + 5) 
+        // crowd funding starts 1 minute laters
+        const startCrowdFund: number = nowInSeconds + 60 * (2 + 5 + 1) 
+        // crow fundings lasts for 10 minutes
+        const endCrowdFund: number = nowInSeconds + 60 * (2 + 5 + 1 + 10) 
+        // 0.001 ETH = 1e15 Wei
+        const minFundValue: number = 1000000000000000; 
+        // for the moment we only allow funding in ETH
+        const allowedFundingTokens: string[] = [] 
+        
+        // deploy Crowd Funding contract
+        const crowdFundContract = await CrowdFundContract.deploy(
+            minFundValue, 
+            allowedFundingTokens, 
+            startRequestFunds, 
+            endRequestFunds, 
+            startCrowdFund, 
+            endCrowdFund
+        );
+        
+        // does request funds period started?
+        let startedRequestFundsPeriodBool: boolean = await crowdFundContract.requestFundsStarted();
+        // does request funds period ended?
+        let endRequestFundsPeriodBool: boolean = await crowdFundContract.requestFundsEnded();
+        // does crowd fund period started?
+        let startCrowdFundPeriodBool: boolean = await crowdFundContract.crowdFundStarted();
+        // does crowd fund period ended?
+        let endCrowdFundPeriodBool: boolean = await crowdFundContract.crowdFundEnded();
+
+        // assert that requesting funds has not yet started
+        assert(!startedRequestFundsPeriodBool);
+        // assert that requesting funds has not yet ended
+        assert(!endRequestFundsPeriodBool);
+        // assert that crowd funding has not yet started
+        assert(!startCrowdFundPeriodBool);
+        // assert that crowd funding has not yet ended
+        assert(!endCrowdFundPeriodBool);
+
+        // test that getTimeLeftRequestFunds() throws an error outside request funds period
+        const getTimeLeftRequestFunds = async (): Promise<void> => {
+            try {
+                await crowdFundContract.getTimeLeftRequestFunds();
+            } catch(err) {
+                const error = err;
+                expect(error).to.be.an("Error");
+            }
+        };
+
+        // expect null output from getTimeLeftRquestFunds == expect(error) to be an error
+        let expectUndefined: void = await getTimeLeftRequestFunds();
+        expect(expectUndefined).to.be.a("undefined");
+
+        // test that getTimeLeftCrowdFund() throws an error outside request funds period
+        const getTimeLeftCrowdFund = async (): Promise<void> => {
+            try {
+                await crowdFundContract.getTimeLeftCrowdFund;
+            } catch(err) {
+                const error = err;
+                expect(error).to.be.an("Error");
+            }
+        }
+
+        // expect null output from getTimeLeftCrowdFund == expect(error) to be an error
+        expectUndefined = await getTimeLeftCrowdFund();
+        expect(expectUndefined).to.be.a("undefined");
+
+        // get latest block number
+        blockNumber = ethers.provider.blockNumber;
+        // get current block data
+        currentBlockData = await ethers.provider.getBlock(blockNumber);
+        // get current block timestamp
+        currentBlockTimestamp = currentBlockData["timestamp"];
+        
+        // we pass forward time to start requesting funds
+        let increaseTimeInSeconds: number = startRequestFunds - currentBlockTimestamp + 1;
+        // request new block with new timestamp of 2 minutes more
+        await ethers.provider.send("evm_increaseTime", [increaseTimeInSeconds]);
+        // request mining new block with this new timestamp
+        await ethers.provider.send("evm_mine", []);
+        
+        startedRequestFundsPeriodBool = await crowdFundContract.requestFundsStarted();
+        endRequestFundsPeriodBool = await crowdFundContract.requestFundsEnded();
+        startCrowdFundPeriodBool = await crowdFundContract.crowdFundStarted();
+        endCrowdFundPeriodBool = await crowdFundContract.crowdFundEnded();
+
+        // requesting funds should have started by now
+        assert(startedRequestFundsPeriodBool);
+        // assert that requesting funds has not yet ended
+        assert(!endRequestFundsPeriodBool);
+        // assert that crowd funding has not yet started
+        assert(!startCrowdFundPeriodBool);
+        // assert that crowd funding has not yet ended
+        assert(!endCrowdFundPeriodBool);
+
+        // testing getTimeLeftRequestFunds() method
+        const timeLeftRequestFunds = await crowdFundContract.getTimeLeftRequestFunds();
+
+        // get latest block number
+        blockNumber = ethers.provider.blockNumber;
+        // get current block data
+        currentBlockData = await ethers.provider.getBlock(blockNumber);
+        // get current block timestamp
+        currentBlockTimestamp = currentBlockData["timestamp"];
+        
+        // get number of seconds from current block timstamp to end request funds time 
+        const endRequestFundsToCurrentTimeBlock: number = endRequestFunds - currentBlockTimestamp;
+        
+        // assert timeLeftRequestFunds == endRequestFundsToCurrentTimeBlock
+        assert(endRequestFundsToCurrentTimeBlock == timeLeftRequestFunds);
+        
+        // get latest block number
+        blockNumber = ethers.provider.blockNumber;
+        // get current block data
+        currentBlockData = await ethers.provider.getBlock(blockNumber);
+        // get current block timestamp
+        currentBlockTimestamp = currentBlockData["timestamp"];
+        
+        // we pass forward time to start requesting funds
+        increaseTimeInSeconds = endRequestFunds - currentBlockTimestamp + 1;
+        // request new block with new timestamp of 2 minutes more
+        await ethers.provider.send("evm_increaseTime", [increaseTimeInSeconds]);
+        // request mining new block with this new timestamp
+        await ethers.provider.send("evm_mine", []);
+        
+        startedRequestFundsPeriodBool = await crowdFundContract.requestFundsStarted(); 
+        endRequestFundsPeriodBool = await crowdFundContract.requestFundsEnded();
+        startCrowdFundPeriodBool = await crowdFundContract.crowdFundStarted();
+        endCrowdFundPeriodBool = await crowdFundContract.crowdFundEnded();  
+        
+        // requesting funds should have started by now
+        assert(startedRequestFundsPeriodBool);
+        // requesting funds should have ended by now
+        assert(endRequestFundsPeriodBool);
+        // assert that crowd funding has not yet started
+        assert(!startCrowdFundPeriodBool);
+        // assert that crowd funding has not yet ended
+        assert(!endCrowdFundPeriodBool);
+
+        // test that getTimeLeftRequestFunds throws an error outside request funds period
+        expectUndefined = await getTimeLeftRequestFunds();
+        expect(expectUndefined).to.be.a("undefined");
+
+        // test that getTimeLeftCrowdFund throws an error outside crowd funds period
+        expectUndefined = await getTimeLeftCrowdFund();
+        expect(expectUndefined).to.be.a("undefined");
+
+        // get latest block number
+        blockNumber = ethers.provider.blockNumber;
+        // get current block data
+        currentBlockData = await ethers.provider.getBlock(blockNumber);
+        // get current block timestamp
+        currentBlockTimestamp = currentBlockData["timestamp"];
+         
+        // we pass forward time to start requesting funds
+        increaseTimeInSeconds = startCrowdFund - currentBlockTimestamp + 1;
+         
+        // request new block with new timestamp of 1 minute more
+        await ethers.provider.send("evm_increaseTime", [increaseTimeInSeconds]);
+        // request mining new block with this new timestamp
+        await ethers.provider.send("evm_mine", []);
+        
+        startedRequestFundsPeriodBool = await crowdFundContract.requestFundsStarted();
+        endRequestFundsPeriodBool = await crowdFundContract.requestFundsEnded();
+        startCrowdFundPeriodBool = await crowdFundContract.crowdFundStarted();
+        endCrowdFundPeriodBool = await crowdFundContract.crowdFundEnded();
+        
+        // requesting funds should have started by now
+        assert(startedRequestFundsPeriodBool);
+        // requesting funds should have ended by now
+        assert(endRequestFundsPeriodBool);
+        // crowd funding should have started by now
+        assert(startCrowdFundPeriodBool);
+        // assert that crowd funding has not yet ended
+        assert(!endCrowdFundPeriodBool);
+
+        // test that getTimeLeftRequestFunds throws an error outside request funds period
+        expectUndefined = await getTimeLeftRequestFunds();
+        expect(expectUndefined).to.be.a("undefined");
+
+        // test getTimeLeftCrowdFund() method
+        const timeLeftCrowdFund: number = await crowdFundContract.getTimeLeftCrowdFund();
+
+        // get latest block number
+        blockNumber = ethers.provider.blockNumber;
+        // get current block data
+        currentBlockData = await ethers.provider.getBlock(blockNumber);
+        // get current block timestamp
+        currentBlockTimestamp = currentBlockData["timestamp"];
+        
+        // get number of seconds from current block timstamp to end request funds time 
+        const endCrowdFundToCurrentBlockTimestamp = endCrowdFund - currentBlockTimestamp;
+        
+        // assert timeLeftRequestFunds == endRequestFundsToCurrentTimeBlock
+        assert(endCrowdFundToCurrentBlockTimestamp == timeLeftCrowdFund);
+        
+        // get latest block number
+        blockNumber = ethers.provider.blockNumber;
+        // get current block data
+        currentBlockData = await ethers.provider.getBlock(blockNumber);
+        // get current block timestamp
+        currentBlockTimestamp = await currentBlockData["timestamp"];
+         
+        // we pass forward time to start requesting funds
+        increaseTimeInSeconds = endCrowdFund - currentBlockTimestamp + 1;
+         
+        // request new block with new timestmap of 10 minutes more 
+        await ethers.provider.send("evm_increaseTime", [increaseTimeInSeconds]);
+        // request mining new block with this new timestamp
+        await ethers.provider.send("evm_mine", []);
+
+        startedRequestFundsPeriodBool = await crowdFundContract.requestFundsStarted();
+        endRequestFundsPeriodBool = await crowdFundContract.requestFundsEnded();
+        startCrowdFundPeriodBool = await crowdFundContract.crowdFundStarted();
+        endCrowdFundPeriodBool = await crowdFundContract.crowdFundEnded();
+        
+        // requesting funds should have started by now
+        assert(startedRequestFundsPeriodBool);
+        // requesting funds should have ended by now
+        assert(endRequestFundsPeriodBool);
+        // crowd funding should have started by now
+        assert(startCrowdFundPeriodBool);
+        // crowd funding should have ended by now
+        assert(endCrowdFundPeriodBool);
+
+        // expect that getTimeLeftRequestFunds throws an error
+        expectUndefined = await getTimeLeftRequestFunds();
+        expect(expectUndefined).to.be.a("undefined");
+
+        // test that getTimeLeftCrowdFund throws an error outside crowd funds period
+        expectUndefined = await getTimeLeftCrowdFund();
+        expect(expectUndefined).to.be.a("undefined");        
+    })
+})
