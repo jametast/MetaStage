5cbc850a9dcdb284054ce9f2dae2426402beb4fe
Ubuntu <ubuntu@ip-172-31-80-213.ec2.internal>
2022-03-25 10:10:43 +0000

    deploy tests refactor to ts

--- meta/deploy/test/crowdFundRefundUsers.ts
+++ meta/deploy/test/crowdFundRefundUsers.ts
@@ -8,6 +8,12 @@ import { getCrowdFundContract, getCurrentBlockTimestamp } from "./deployTest";
 describe("CrowdFundRefundUsers", function () {
     it("Refuding users that voted for creators which didn't get enough funds", async function () {
     
-        let { owner, creator1, creator2, user1, user2, user3, user4 }: Signer = ethers.getSigners();
+        let accounts: Signer[] = await ethers.getSigners();
+        let owner: Signer = accounts[0];
+        let creator1: Signer = accounts[1];
+        let creator2: Signer = accounts[2];
+        let user1: Signer = accounts[3];
+        let user2: Signer = accounts[4];
+        let user3: Signer = accounts[5];
     });
 });
---
+++ meta/deploy/test/deployTest.ts
@@ -0,0 +1,52 @@
+import { expect, assert } from "chai";
+import { ethers } from "hardhat";
+import { BigNumber, ContractFactory, Contract, providers } from "ethers";
+
+
+const getCrowdFundContract = async (): Promise<Contract> => {
+    const CrowdFundContract: ContractFactory = await ethers.getContractFactory("CrowdFundContract");
+    
+    // get current block number
+    let blockNumber: number = ethers.provider.blockNumber;
+    // get current block data
+    let currentBlockData: providers.Block = await ethers.provider.getBlock(blockNumber);
+    // get current block timestamp
+    let currentBlockTimestamp: number = currentBlockData["timestamp"];
+    
+    // get current timestamp in seconds 
+    const nowInSeconds: number = currentBlockTimestamp + 1; // prior, we were testing it with date now method:  Math.floor(Date.now() / 1000);
+    // starts in 2 minutes
+    const startRequestFunds: number = nowInSeconds + 60 * 2;
+    // requesting funds will have a duration of 5 minutes
+    const endRequestFunds: number = nowInSeconds + 60 * (2 + 5);
+    // crowd funding starts 1 minute laters
+    const startCrowdFund: number = nowInSeconds + 60 * (2 + 5 + 1);
+    // crow fundings lasts for 10 minutes
+    const endCrowdFund: number = nowInSeconds + 60 * (2 + 5 + 1 + 10);
+    // 0.001 ETH = 1e15 Wei
+    const minFundValue: BigNumber = ethers.utils.parseEther('0.01'); 
+    // for the moment we only allow funding in ETH
+    const allowedFundingTokens: any[] = [];
+    
+    // deploy Crowd Funding contract
+    const crowdFundContract: Contract = await CrowdFundContract.deploy(
+        minFundValue, 
+        allowedFundingTokens, 
+        startRequestFunds, 
+        endRequestFunds, 
+        startCrowdFund, 
+        endCrowdFund
+    );
+    
+    return crowdFundContract;
+}
+
+
+const getCurrentBlockTimestamp = async (): Promise<number> => {
+    const blockNumber: number = ethers.provider.blockNumber;
+    const currentBlockData: providers.Block = await ethers.provider.getBlock(blockNumber);
+    const currentTimestamp: number = currentBlockData["timestamp"];
+    return currentTimestamp;
+}
+
+export { getCrowdFundContract, getCurrentBlockTimestamp };
