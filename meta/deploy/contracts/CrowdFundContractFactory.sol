c9f7dabf52254bc7bce78fabcd1527239c8a7da1
Ubuntu <matroid@outlook.com>
2022-05-14 21:20:36 +0000

    refactor crowd fund contract factory

--- meta/deploy/contracts/CrowdFundContractFactory.sol
+++ meta/deploy/contracts/CrowdFundContractFactory.sol
@@ -17,6 +17,8 @@ contract CrowdFundContractFactory is Ownable {
     uint256 private _currentRoundId; // current round Id
     event CrowdFundContractCreated(address crowdFundContractAddress); // event announcing clone CrowdFundContract creation
 
+    error invalidRoundId(); 
+
     constructor(address crowdFundContractMasterAddress) onlyOwner public {
         _currentRoundId = 0; // no contract deployed yet
         _crowdFundContractMasterAddress = crowdFundContractMasterAddress; // our initial master crowd fund contract, it can be updated later on
@@ -65,5 +67,75 @@ contract CrowdFundContractFactory is Ownable {
         roundIdToCrowdFundContractAddressMapping[_currentRoundId] = cloneCrowdFundContractAddress; 
 
         return cloneCrowdFundContractAddress;
+    }
+
+    /**
+    * Given a roundId, the current contract will deploy a new crowd fund contract attached to this
+    * roundId, therefore, the current contract will be the onlyOwner of such crowd fund contract
+    * for this reason, we have to reimplement every method that it is owned by the current smart contract,
+    * in order to be able to interact from the external (and actual) owner of the contract
+    */
+
+    // set the price feed contract of the roundId => crowd fund contract
+    function setPriceFeedContract(address _token, address _priceFeed, uint256 _roundId) public onlyOwner {
+        // we require that the given round id is valid 
+        if (_roundId > _currentRoundId) {
+            revert invalidRoundId();
+        }
+        address crowdFundContractAddress = roundIdToCrowdFundContractAddressMapping[_roundId];
+        CrowdFundContract crowdFundContract = CrowdFundContract(crowdFundContractAddress);
+        crowdFundContract.setPriceFeedContract(_token, _priceFeed);
+    }
+
+    // make a creator elligible for roundId => crowd fund contract 
+    function makeCreatorElligible(address _wallet, uint256 _roundId) public onlyOwner {
+        // we require that the given round id is valid 
+        if (_roundId > _currentRoundId) {
+            revert invalidRoundId();
+        }
+        address crowdFundContractAddress = roundIdToCrowdFundContractAddressMapping[_roundId];
+        CrowdFundContract crowdFundContract = CrowdFundContract(crowdFundContractAddress);
+        crowdFundContract.makeCreatorElligible(_wallet);
+    }
+
+    // fund creators for roundId => crowd fund contract
+    function fundCreators(address _wallet, uint256 _roundId) payable onlyOwner public {
+        // we require that the given round id is valid
+        if (_roundId > _currentRoundId) {
+            revert invalidRoundId();
+        }
+        address crowdFundContractAddress = roundIdToCrowdFundContractAddressMapping[_roundId];
+        CrowdFundContract crowdFundContract = CrowdFundContract(crowdFundContractAddress);
+        crowdFundContract.fundCreators(_wallet);
+    }
+
+    // refund users for roundId => crowd fund contract
+    function refundUsers(uint256 _roundId) payable onlyOwner public {
+        // we require that the given round id is valid 
+        if (_roundId > _currentRoundId) {
+            revert invalidRoundId();
+        }
+        address crowdFundContractAddress = roundIdToCrowdFundContractAddressMapping[_roundId];
+        CrowdFundContract crowdFundContract = CrowdFundContract(crowdFundContractAddress);
+        crowdFundContract.refundUsers();
+    }
+
+    // fund meta nft minting contract for roundId => crowd fund contract
+    function fundMetaNFTMintContract(
+        address creatorAddress, 
+        address payable metaNFTMintContractAddress,
+        uint256 _roundId
+    ) 
+        payable 
+        onlyOwner 
+        public 
+    {   
+        // we require that the given round id is valid 
+        if (_roundId > _currentRoundId) {
+            revert invalidRoundId();
+        }
+        address crowdFundContractAddress = roundIdToCrowdFundContractAddressMapping[_roundId];
+        CrowdFundContract crowdFundContract = CrowdFundContract(crowdFundContractAddress);
+        crowdFundContract.fundMetaNFTMintContract(creatorAddress, metaNFTMintContractAddress);
     }
 }
